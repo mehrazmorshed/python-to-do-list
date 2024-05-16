6ba5d64d0c01ec40cacffc4355ddf8f36812be23
Mehraz Morshed <mehrazmorshed@gmail.com>
2024-05-16 12:26:43 +0600

    Define the main menu
    Define the main menu

--- todo_list.py
+++ todo_list.py
@@ -1,1 +1,7 @@
-
+def display_menu():
+    print("\nTo-Do List Application")
+    print("1. Add a task")
+    print("2. View tasks")
+    print("3. Mark a task as done")
+    print("4. Remove a task")
+    print("5. Exit")
